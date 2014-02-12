2009b914f442771ac2b7f25015d2e1daa8fa9201
Cyberarm <matthewlikesrobots@gmail.com>
2014-02-12 12:20:00 -0600

    Added start of asset packs. #22

--- lib/planet-wars/asset_manager.rb
+++ lib/planet-wars/asset_manager.rb
@@ -1,36 +1,44 @@
 module AssetManager
+  def self.asset_pack
+    # TODO: Add ConfigManager
+    @config_data = Psych.load_file("./lib/planet-wars/game/data/config.yml")
+    @config_data["config"]["asset_pack"]
+  end
   def self.portal_path
-    "./assets/portal"
+    "./assets/#{asset_pack}/portal"
   end
   def self.ships_path
-    "./assets/ships"
+    "./assets/#{asset_pack}/ships"
   end
   def self.planets_path
-    "./assets/planets"
+    "./assets/#{asset_pack}/planets"
   end
   def self.asteroids_path
-    "./assets/asteroids"
+    "./assets/#{asset_pack}/asteroids"
   end
   def self.bullets_path
-    "./assets/bullets"
+    "./assets/#{asset_pack}/bullets"
+  end
+  def self.background_path
+    "./assets/#{asset_pack}/backgrounds"
   end
   def self.music_path
-    "./assets/music"
+    "./assets/#{asset_pack}/music"
   end
   def self.sounds_path
-    "./assets/sounds"
+    "./assets/#{asset_pack}/sounds"
   end
   def self.fonts_path
-    "./assets/fonts"
+    "./assets/#{asset_pack}/fonts"
   end
 
   def self.credits_data
-    @credits_data = Psych.load_file("./assets/data/credits.yml") unless defined?(@credits_data)
+    @credits_data = Psych.load_file("./assets/#{asset_pack}/data/credits.yml") unless defined?(@credits_data)
     @credits_data
   end
 
   def self.preload_assets
-    @credits_data = Psych.load_file("./assets/data/credits.yml")
+    @credits_data = Psych.load_file("./assets/#{asset_pack}/data/credits.yml")
     images = []
     music  = []
     Dir[portal_path+'/*.png'].each {|image| images << image}
--- lib/planet-wars/objects/background.rb
+++ lib/planet-wars/objects/background.rb
@@ -1,6 +1,6 @@
 class Background < Chingu::GameObject
   def setup
-    @image = Gosu::Image["assets/backgrounds/background-04.png"]
+    @image = Gosu::Image["#{AssetManager.background_path}/background.png"]
     self.scale = 2.0
   end
 end
